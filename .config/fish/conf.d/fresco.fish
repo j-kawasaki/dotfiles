6c64b09d8de77f5c08a5a3d4446ae3ea7de45c3e
j-kawasaki <j-kawasaki@ist.osaka-u.ac.jp>
2019-05-16 02:36:39 +0000

    reset

--- .config/fish/conf.d/fresco.fish
+++
@@ -1,7 +0,0 @@
-set -l bootstrap_file /mnt/d/j-kawasaki/.local/share/fresco/repos/github.com/masa0x80/fresco/fresco.fish
-if test -r $bootstrap_file
-    source $bootstrap_file
-else
-    mkdir -p /mnt/d/j-kawasaki/.local/share/fresco/repos/github.com/masa0x80/fresco
-    git clone https://github.com/masa0x80/fresco /mnt/d/j-kawasaki/.local/share/fresco/repos/github.com/masa0x80/fresco
-end
